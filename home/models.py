03dbf96ee3618780f250da43d1ed797c68340fa4
Godwin Ofwono <godwinofwono933@gmail.com>
2025-03-29 16:46:26 +0300

    Worked on The last name Column that was missing in the database

--- home/models.py
+++ home/models.py
@@ -5,7 +5,7 @@ class trial(models.Model):
     title =models.CharField(max_length=225)
     author =models.CharField(max_length=45)
     fname =models.CharField(max_length=50,default='First Name')
-    fname =models.CharField(max_length=50,default='Last Name')
+    lname =models.CharField(max_length=50,default='Last Name')
     email =models.EmailField(max_length=100,default='example@gmil.com')
     age =models.IntegerField()
     content =models.TextField()
